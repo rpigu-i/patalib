4ba4d0facdf6f068570c795f186a3e2db74e1047
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2020-06-11 14:01:37 -0400

    Up dependencies in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='patalib',
-    version='1.0.1',
+    version='2.0.0',
     description='Pataphysics library',
     maintainer='patamechanix',
     license='MIT',
@@ -12,8 +12,8 @@ setup(
     include_package_data=True,
     packages=find_packages('src'),
     install_requires = [
-        'nltk==2.0.5',
-        'hypothesis==3.4.2'
+        'nltk>=3.4.5',
+        'hypothesis>=5.16.0'
 
     ]
 )
